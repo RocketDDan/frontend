f8373bf8a47559506d799f74bb5948400d04bc52
JaeY0ungJung <hanol5216@gmail.com>
2025-06-09 20:51:40 +0900

    feat: 3개의 input 컴퍼넌트 생성

--- src/components/base/Input.jsx
+++ src/components/base/Input.jsx
@@ -2,7 +2,15 @@ import { v7 as uuidv7 } from 'uuid';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import { useState } from 'react';
+import InputStyle from "./Input.module.css";
 
+/**
+ * Text Input Component
+ * @param {Object} props 
+ * @param {String} props.placeholder placeholder
+ * @param {String} props.width width
+ * @returns 
+ */
 const TextInput = (props) => {
     const [value, setValue] = useState(props.value || "");
     const handleChange = (e) => {
@@ -13,14 +21,7 @@ const TextInput = (props) => {
     };
 
     return (
-        <span style={{
-            textAlign: "start",
-            display: "flex",
-            flexDirection: "column",
-            gap: "0.3rem",
-            width: props.width || "100%",
-            position: "relative",
-        }}>
+        <span className={InputStyle.container} style={{ width: props.width || "100%" }}>
             <input
                 type="text"
                 placeholder={props.placeholder || ""}
@@ -51,7 +52,14 @@ const TextInput = (props) => {
     )
 }
 
-const TextInputWithLabels = (props) => {
+/**
+ * Text Input With Label Component
+ * @param {Object} props 
+ * @param {String} props.placeholder placeholder
+ * @param {String} props.width width
+ * @returns 
+ */
+const TextInputWithLabel = (props) => {
     const id = uuidv7();
     const [value, setValue] = useState(props.value || "");
 
@@ -64,16 +72,7 @@ const TextInputWithLabels = (props) => {
     };
 
     return (
-        <span
-            style={{
-                textAlign: "start",
-                display: "flex",
-                flexDirection: "column",
-                gap: "0.3rem",
-                width: props.width || "100%",
-                position: "relative",
-            }}
-        >
+        <span className={InputStyle.container} style={{ width: props.width || "100%" }}>
             <label htmlFor={id}>{props.label || "label"}</label>
             <input
                 id={id}
@@ -106,4 +105,55 @@ const TextInputWithLabels = (props) => {
     )
 }
 
-export { TextInput, TextInputWithLabels }
+/**
+ * TextArea With Label Component
+ * @param {Object} props 
+ * @param {String} props.placeholder placeholder
+ * @param {String} props.width width
+ * @returns 
+ */
+const TextAreaWithLabel = (props) => {
+    const id = uuidv7();
+    const [value, setValue] = useState(props.value || "");
+
+    const handleChange = (e) => {
+        setValue(e.target.value);
+    };
+
+    const clearInput = () => {
+        setValue("");
+    };
+
+    return (
+        <span className={InputStyle.container} style={{ width: props.width || "100%" }}>
+            <label htmlFor={id}>{props.label || "label"}</label>
+            <textarea
+                id={id}
+                placeholder={props.placeholder || ""}
+                value={value}
+                onChange={handleChange}
+                style={{
+                    padding: "0.7rem 0 0.7rem 0.7rem",
+                    borderRadius: "8px",
+                    border: "solid 1px",
+                    width: "100%",
+                }}
+            />
+            {value && (
+                <FontAwesomeIcon
+                    icon={faTimes}
+                    onClick={clearInput}
+                    style={{
+                        position: "absolute",
+                        right: "10px",
+                        top: "2.2rem",
+                        cursor: "pointer",
+                        color: "#999",
+                    }}
+                />
+            )}
+        </span>
+    )
+}
+
+export { TextInput, TextInputWithLabel, TextAreaWithLabel }
