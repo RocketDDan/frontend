aede1a870967633bfff72abfd45e10e1bac20bb8
JaeY0ungJung <hanol5216@gmail.com>
2025-06-09 20:51:17 +0900

    feat: 4개의 버튼 컴퍼넌트 생성

--- src/components/base/Button.jsx
+++ src/components/base/Button.jsx
@@ -1,19 +1,69 @@
+import ButtonStyle from "./Button.module.css"
+import CommonStyle from "../../Common.module.css"
 
-// Parameter: content
+/**
+ * Primary 버튼 컴포넌트
+ * @component
+ * @param {Object} props
+ * @param {String} props.content 버튼 내 글자
+ * @param {String} props.width 버튼 가로 길이
+ * @returns {JSX.Element} 프로필 이미지 컴포넌트
+ */
 const PrimaryButton = (props) => {
     return (
-        <button style={{
-            textAlign: "center",
-            background: "blue",
-            color: "white",
-            padding: "10px 20px",
-            borderRadius: "8px",
-            border: "none",
-            cursor: "pointer",
-        }}>
+        <button style={{ width: props.width || '100%' }} className={`${ButtonStyle.container} ${CommonStyle.primaryBg}`}>
             {props.content || "버튼"}
         </button>
     )
 }
 
-export { PrimaryButton };
+
+/**
+ * Secondary 버튼 컴포넌트
+ * @component
+ * @param {Object} props
+ * @param {String} props.content 버튼 내 글자
+ * @param {String} props.width 버튼 가로 길이
+ * @returns {JSX.Element} 프로필 이미지 컴포넌트
+ */
+const SecondaryButton = (props) => {
+    return (
+        <button style={{ width: props.width || '100%' }} className={`${ButtonStyle.container} ${CommonStyle.secondaryBg}`} >
+            {props.content || "버튼"}
+        </button>
+    )
+}
+
+/**
+ * Primary Big 버튼 컴포넌트
+ * @component
+ * @param {Object} props
+ * @param {String} props.content 버튼 내 글자
+ * @param {String} props.width 버튼 가로 길이
+ * @returns {JSX.Element} 프로필 이미지 컴포넌트
+ */
+const PrimaryBigButton = (props) => {
+    return (
+        <button style={{ width: props.width || '100%' }} className={`${ButtonStyle.container} ${ButtonStyle.big} ${CommonStyle.primaryBg}`}>
+            {props.content || "버튼"}
+        </button>
+    )
+}
+
+/**
+ * Secondary Big 버튼 컴포넌트
+ * @component
+ * @param {Object} props
+ * @param {String} props.content 버튼 내 글자
+ * @param {String} props.width 버튼 가로 길이
+ * @returns {JSX.Element} 프로필 이미지 컴포넌트
+ */
+const SecondaryBigButton = (props) => {
+    return (
+        <button style={{ width: props.width || '100%' }} className={`${ButtonStyle.container} ${ButtonStyle.big} ${CommonStyle.secondaryBg}`}>
+            {props.content || "버튼"}
+        </button>
+    )
+}
+
+export { PrimaryButton, SecondaryButton, PrimaryBigButton, SecondaryBigButton };
