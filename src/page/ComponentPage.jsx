e5cf7e1a91bfed1c635b7be588ec3aba94dd7408
JaeY0ungJung <hanol5216@gmail.com>
2025-06-10 12:00:22 +0900

    add: 추가 생성된 select, radio 컴퍼넌트 화면에 추가

--- src/page/ComponentPage.jsx
+++ src/page/ComponentPage.jsx
@@ -1,4 +1,5 @@
 import { TextAreaWithLabel, TextInput, TextInputWithLabel } from "../components/base/Input";
+import { BasicCheckbox } from "../components/base/Checkbox";
 import { FeedProfileImage, CrewProfileImage, MemberProfileImage } from "../components/profile/ProfileImage";
 import { PrimaryBigButton, PrimaryButton, SecondaryBigButton, SecondaryButton } from "../components/base/Button";
 import { BasicTable, TableWithNoLine } from "../components/base/Table";
@@ -7,6 +8,10 @@ import { SquareFeed } from "../components/feed/SquareFeed";
 import MetamongImage from "../assets/images/metamong.jpeg"
 import Coffee from '../example/Coffee';
 import Number from "../example/Number";
+import { BasicRadio } from "../components/base/Radio";
+import Select from "../example/Select";
+import { BasicSelect } from "../components/base/Select";
+import Calculate from "../example/Calculate";
 
 const HomePage = () => {
     return (
@@ -20,64 +25,99 @@ const HomePage = () => {
                 {/* INPUT */}
                 <h2>Input 태그</h2>
                 <div style={{ display: 'flex', justifyContent: 'start', gap: '5rem' }}>
-                    <div style={{ flex: '1'}}>
+                    <div style={{ flex: '1' }}>
                         <h4>Text Input</h4>
                         <TextInput />
                     </div>
-                    <div style={{ flex: '1'}}>
+                    <div style={{ flex: '1' }}>
                         <h4>Text Input With Label</h4>
                         <TextInputWithLabel />
                     </div>
-                    <div style={{ flex: '1'}}>
+                    <div style={{ flex: '1' }}>
                         <h4>Text Area With Label</h4>
                         <TextAreaWithLabel />
                     </div>
                 </div>
-                <hr style={{ border: "none", borderBottom: "1px solid #ccc", margin: "8px 0" }}/>
+                <hr style={{ border: "none", borderBottom: "1px solid #ccc", margin: "8px 0" }} />
+
+                {/* check box & radio */}
+                <h2>Checkbox & Radio 태그</h2>
+                <div style={{ display: 'flex', justifyContent: 'start', gap: '5rem' }}>
+                    <div>
+                        <h4>Checkbox 태그</h4>
+                        <BasicCheckbox content="체크하세요" />
+                    </div>
+                    <div>
+                        <h4>Radio 태그</h4>
+                        <BasicRadio content="옵션1" name="option" value="value" />
+                        <BasicRadio content="옵션2" name="option" value="value" defaultChecked={true} />
+                        <BasicRadio content="옵션3" name="option" value="value" />
+                    </div>
+                </div>
+                <hr style={{ border: "none", borderBottom: "1px solid #ccc", margin: "8px 0" }} />
+
+                {/* select  */}
+                <h2>Select 태그</h2>
+                <div style={{ display: 'flex', justifyContent: 'start', gap: '5rem' }}>
+                    <div>
+                        <h4>Select 태그</h4>
+                        <BasicSelect
+                            value="apple"
+                            onChange={alert}
+                            options={[
+                                { value: "orange", label: "오렌지" },
+                                { value: "apple", label: "사과" },
+                            ]} />
+                    </div>
+                </div>
+                <hr style={{ border: "none", borderBottom: "1px solid #ccc", margin: "8px 0" }} />
+
                 {/* 프로필 이미지 */}
                 <h2>프로필 이미지 태그</h2>
                 <div style={{ display: 'flex', justifyContent: 'start', gap: '5rem' }}>
-                    <div style={{ flex: '1'}}>
+                    <div style={{ flex: '1' }}>
                         <h4>Feed Profile Image</h4>
                         <FeedProfileImage />
                     </div>
-                    <div style={{ flex: '1'}}>
+                    <div style={{ flex: '1' }}>
                         <h4>Crew Profile Image</h4>
                         <CrewProfileImage />
                     </div>
-                    <div style={{ flex: '1'}}>
+                    <div style={{ flex: '1' }}>
                         <h4>Member Profile Image</h4>
                         <MemberProfileImage />
                     </div>
                 </div>
-                <hr style={{ border: "none", borderBottom: "1px solid #ccc", margin: "8px 0" }}/>
+                <hr style={{ border: "none", borderBottom: "1px solid #ccc", margin: "8px 0" }} />
+
                 {/* 버튼 */}
                 <h2>button 태그</h2>
                 <div style={{ display: 'flex', justifyContent: 'start', gap: '5rem' }}>
-                    <div style={{ flex: '1'}}>
+                    <div style={{ flex: '1' }}>
                         <h4>Primary Button (90px)</h4>
                         <PrimaryButton width="90px" />
                     </div>
-                    <div style={{ flex: '1'}}>
+                    <div style={{ flex: '1' }}>
                         <h4>Secondary Button (90px)</h4>
                         <SecondaryButton width="90px" />
                     </div>
                 </div>
                 <div style={{ display: 'flex', justifyContent: 'start', gap: '5rem' }}>
-                    <div style={{ flex: '1'}}>
+                    <div style={{ flex: '1' }}>
                         <h4>Primary Big Button (120px)</h4>
                         <PrimaryBigButton width="120px" />
                     </div>
-                    <div style={{ flex: '1'}}>
+                    <div style={{ flex: '1' }}>
                         <h4>Secondary Big Button (120px)</h4>
                         <SecondaryBigButton width="120px" />
                     </div>
                 </div>
-                <hr style={{ border: "none", borderBottom: "1px solid #ccc", margin: "8px 0" }}/>
+                <hr style={{ border: "none", borderBottom: "1px solid #ccc", margin: "8px 0" }} />
+
                 {/* 테이블 */}
                 <h2>table 태그</h2>
                 <div style={{ display: 'flex', justifyContent: 'start', gap: '5rem' }}>
-                    <div style={{ flex: '1'}}>
+                    <div style={{ flex: '1' }}>
                         <h4>테이블</h4>
                         <BasicTable
                             headers={['컬럼1', '컬럼2', '컬럼3', '컬럼4']}
@@ -91,7 +131,7 @@ const HomePage = () => {
                             ]}
                         />
                     </div>
-                    <div style={{ flex: '1'}}>
+                    <div style={{ flex: '1' }}>
                         <h4>선 없는 테이블</h4>
                         <TableWithNoLine
                             headers={['컬럼1', '컬럼2', '컬럼3', '컬럼4']}
@@ -106,17 +146,21 @@ const HomePage = () => {
                         />
                     </div>
                 </div>
-                <hr style={{ border: "none", borderBottom: "1px solid #ccc", margin: "8px 0" }}/>
+                <hr style={{ border: "none", borderBottom: "1px solid #ccc", margin: "8px 0" }} />
+
                 {/* 피드 */}
                 <h2>피드 태그</h2>
                 <div>
                     <h4>사각 피드 (140px)</h4>
                     <SquareFeed url={MetamongImage} />
                 </div>
-                <hr style={{ border: "none", borderBottom: "1px solid #ccc", margin: "8px 0" }}/>
+                <hr style={{ border: "none", borderBottom: "1px solid #ccc", margin: "8px 0" }} />
+
                 {/* 예제 */}
                 <Coffee />
                 <Number />
+                <Select fruit='apple' />
+                <Calculate/>
             </div>
         </div>
     )
