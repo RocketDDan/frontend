e83d415b224cf87db42b471b3356160da010fc2a
JaeY0ungJung <hanol5216@gmail.com>
2025-06-10 09:54:26 +0900

    feat: 헤더의 오른쪽 부분을 '로그아웃 버튼'에서 '프로필 이미지와 닉네임'으로 수정하고, 클릭 시 펴지도록 변경

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,10 +1,19 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import headerStyle from './Header.module.css';
+import { LoginMemberProfileImage } from '../profile/ProfileImage'
+import MetamongImage from "../../assets/images/metamong.jpeg"
 
 const Header = () => {
     const [menuOpen, setMenuOpen] = useState(false);
-    const userRole = 'ANONYMOUS'; // TODO: 어디선가 빼올 예정
+    const [profileOpen, setProfileOpen] = useState(false);
+
+    const user = {
+        nickname: "러너스하이",
+        role: 'USER', // TODO: 어디선가 빼올 예정 (ANONYMOUS, USER, ADMIN, COMPANY)
+    }
+
+    const toggleProfileMenu = () => setProfileOpen(prev => !prev);
 
     return (
         <header className={headerStyle.container}>
@@ -14,10 +23,25 @@ const Header = () => {
                 </h1>
 
                 <nav className={headerStyle.desktopNav}>
-                    {userRole === 'ANONYMOUS' ? (
+                    {user.role === 'ANONYMOUS' ? (
                         <Link to="/login">로그인</Link>
                     ) : (
-                        <Link to="/logout">로그아웃</Link>
+                        <div className={headerStyle.profileWrapper}>
+                            <div className={headerStyle.profileContainer} onClick={toggleProfileMenu}>
+                                <LoginMemberProfileImage profileUrl={MetamongImage} />
+                                <span>
+                                    <span className={headerStyle.nickname}>{user.nickname}</span>
+                                    <span className={headerStyle.nim}>님</span>
+                                </span>
+                                {/* <Link to="/logout">로그아웃</Link> */}
+                            </div>
+                            {profileOpen && (
+                                <div className={headerStyle.dropdownMenu}>
+                                    <Link to="/account/setting">내 정보 수정</Link>
+                                    <Link to="/logout">로그아웃</Link>
+                                </div>
+                            )}
+                        </div>
                     )}
                 </nav>
 
