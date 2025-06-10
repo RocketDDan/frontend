f57a24f8638fb85955bf0afa39d51e3815b48541
JaeY0ungJung <hanol5216@gmail.com>
2025-06-10 10:10:34 +0900

    feat: 정사각형 피드 컴퍼넌트 생성

---
+++ src/components/feed/SquareFeed.jsx
@@ -0,0 +1,18 @@
+
+/**
+ * 정사각형 피드
+ * @component
+ * @param {Object} props 
+ * @param {String} props.url
+ * @returns {JSX.Element} 정사각형 피드 컴포넌트
+ */
+const SquareFeed = (props) => {
+    const length = '140px';
+    return (
+        <div style={{ width: length, height: length }}>
+            <img src={props.url} alt="피드 대표 이미지 url" width={length} />
+        </div>
+    )
+}
+
+export default SquareFeed;
