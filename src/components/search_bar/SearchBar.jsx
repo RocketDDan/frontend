b872d3eaaf58ca703e5c4803791fa3331ec1cda9
JaeY0ungJung <hanol5216@gmail.com>
2025-06-10 12:19:20 +0900

    feat: 검색창 컴퍼넌트 생성

---
+++ src/components/search_bar/SearchBar.jsx
@@ -0,0 +1,36 @@
+import searchBarStyle from "./SearchBar.module.css";
+
+import { useState } from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes, faSearch } from "@fortawesome/free-solid-svg-icons";
+
+
+const SearchBar = (props) => {
+
+    const [value, setValue] = useState(props.value || "");
+    const handleChange = (e) => setValue(e.target.value);
+    const clearInput = () => setValue("");
+
+    return (
+        <label className={searchBarStyle.container} style={{ width: props.width || "100%" }}>
+            <FontAwesomeIcon icon={faSearch} className={searchBarStyle.searchBtn}/>
+            <input
+                type="text"
+                placeholder={props.placeholder || ""}
+                value={value}
+                onChange={handleChange}
+                maxLength={50}
+            />
+            {value && (
+                <FontAwesomeIcon
+                    icon={faTimes}
+                    onClick={clearInput}
+                    className={searchBarStyle.closeBtn}
+                />
+            )}
+        </label>
+    )
+};
+
+export { SearchBar };
