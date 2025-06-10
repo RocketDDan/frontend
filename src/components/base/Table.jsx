20cb898d7db6c1d988d1671d4f2203b67e956b04
JaeY0ungJung <hanol5216@gmail.com>
2025-06-10 10:53:13 +0900

    feat: 선 없는 테이블 생성

--- src/components/base/Table.jsx
+++ src/components/base/Table.jsx
@@ -18,6 +18,30 @@ const BasicTable = (props) => {
 
     return (
         <table style={{ width: props.width || '100%', }}>
+            <tr className={`${tableStyle.row} ${tableStyle.borderButtom}`}>
+                {headers.map((header, idx) => (
+                    <td key={idx} className={`${tableStyle.row} ${tableStyle.borderButtom}`}>{header}</td>
+                ))}
+            </tr>
+            {data.map((d, rowIdx) => (
+                <tr key={rowIdx} className={`${tableStyle.row} ${tableStyle.borderButtom}`}>
+                    {keys.map((key, colIdx) => (
+                        <td key={colIdx} className={`${tableStyle.row} ${tableStyle.borderButtom}`}>{d[key]}</td>
+                    ))}
+                </tr>
+            ))}
+        </table>
+    )
+}
+
+const TableWithNoLine = (props) => {
+
+    const headers = props.headers;
+    const keys = props.keys;
+    const data = props.data;
+
+    return (
+        <table style={{ width: props.width || '100%', }}>
             <tr className={tableStyle.row}>
                 {headers.map((header, idx) => (
                     <td key={idx} className={tableStyle.row}>{header}</td>
@@ -33,4 +57,4 @@ const BasicTable = (props) => {
         </table>
     )
 }
-export { BasicTable };
+export { BasicTable, TableWithNoLine };
