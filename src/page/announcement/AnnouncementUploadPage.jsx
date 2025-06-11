b6586f7f6ce9024fb320b9d1f37926d487035dfa
Soojin-Lee-01 <sojinlee1004@naver.com>
2025-06-11 16:44:05 +0900

    feat: 공지사항 업로드 페이지 레이아웃 1차 구현

--- src/page/announcement/AnnouncementUploadPage.jsx
+++ src/page/announcement/AnnouncementUploadPage.jsx
@@ -1,9 +1,34 @@
+import { TextAreaWithLabel, TextInput } from "../../components/base/Input";
+import styles from "./AnnouncementUploadPage.module.css";
 const AnnouncementUploadPage = () => {
     
     return (
-        <div>
-            <h1>공지 업로그</h1>
-        </div>
+        <div className={styles.container}>
+            <h1>공지사항 업로드</h1>
+
+            <div className={styles.formGroup}>
+                <label>제목</label>
+                <TextInput />
+            </div>
+        
+            <div className={styles.formGroup}>
+                <label>첨부 파일</label>
+                <div className={styles.row}>
+                    <div className={styles.fileInputWrapper}>
+                    <input type="text" className={styles.textInput} />
+                    </div>
+                    <button className={styles.fileBtn}>내 PC</button>
+                </div>
+                </div>
+
+                <div className={styles.formGroup}>
+                <label>본문</label>
+                <TextInput />
+                </div>
+
+            <button className={styles.submitBtn}>업로드</button>
+            </div>
+
     )
 }
 
