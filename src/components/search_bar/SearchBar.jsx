54ee9d2a28031d098c902feacce94fa0d5dbf0e9
JaeY0ungJung <hanol5216@gmail.com>
2025-06-12 09:36:28 +0900

    fix: 공통 component 수정 및 click, change 이벤트 추가

--- src/components/search_bar/SearchBar.jsx
+++ src/components/search_bar/SearchBar.jsx
@@ -5,27 +5,37 @@ import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes, faSearch } from "@fortawesome/free-solid-svg-icons";
 
+/**
+ * 
+ * @param {String} placeholder placeholder
+ * @param {String} defaultValue 기본 값
+ * @param {String} width 가로 길이
+ * @param {Function} onChange 가로 길이
+ * @returns {JSX.Element} search bar 컴퍼넌트
+ */
+const SearchBar = ({ placeholder = "", defaultValue, width = "100%", onChange }) => {
 
-const SearchBar = (props) => {
+    const [inValue, setInValue] = useState(defaultValue || "");
 
-    const [value, setValue] = useState(props.value || "");
-    const handleChange = (e) => setValue(e.target.value);
-    const clearInput = () => setValue("");
+    const handleChange = (val) => {
+        setInValue(val);
+        onChange?.(val);
+    }
 
     return (
-        <label className={searchBarStyle.container} style={{ width: props.width }}>
-            <FontAwesomeIcon icon={faSearch} className={searchBarStyle.searchBtn}/>
+        <label className={searchBarStyle.container} style={{ width: width }}>
+            <FontAwesomeIcon icon={faSearch} className={searchBarStyle.searchBtn} />
             <input
                 type="text"
-                placeholder={props.placeholder || ""}
-                value={value}
-                onChange={handleChange}
+                placeholder={placeholder}
+                value={inValue}
+                onChange={(e) => { handleChange(e.target.value) }}
                 maxLength={50}
             />
-            {value && (
+            {inValue && (
                 <FontAwesomeIcon
                     icon={faTimes}
-                    onClick={clearInput}
+                    onClick={() => { handleChange("") }}
                     className={searchBarStyle.closeBtn}
                 />
             )}
