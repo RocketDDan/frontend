8377453c9d2d86f57e29beb8346797df595c2f4e
Soojin-Lee-01 <sojinlee1004@naver.com>
2025-06-12 16:47:45 +0900

    feat: 페이지네이션 구현 및 스타일 작성

--- src/components/base/AnnouncementTable.jsx
+++ src/components/base/AnnouncementTable.jsx
@@ -1,6 +1,7 @@
 import AcTableStyle from "./AnnouncementTable.module.css"
 import { useState, useEffect } from "react";
 import axios from "axios";
+import Pagination from "../announcement/Pagination";
 
 const TableHeader = ({headers}) => (
     <thead>
@@ -17,22 +18,23 @@ const TableHeader = ({headers}) => (
 );
 
 
-const TableBody = ({ data, keys }) => (
+const TableBody = ({ data, keys, page, limit }) => (
     <tbody>
         {data.map((row, rowIdx) => (
             <tr key={rowIdx} className={AcTableStyle.tr}>
-                <td className={AcTableStyle.td}>{rowIdx + 1}</td>
+                <td className={AcTableStyle.td}>
+                    {(page-1) * limit + rowIdx + 1}
+                </td>
                 {keys.map((key, colIdx) => (
                     <td
                         key={colIdx}
                         className={
-                            key === 'crewName'
-                                ? AcTableStyle.role
+                            key === 'crewName' && row[key] === '관리자'
+                                ? `${AcTableStyle.td} ${AcTableStyle.role}`
                                 : key === 'title'
-                                    ? `${AcTableStyle.td} ${AcTableStyle.wideColumn}` 
-                                    : AcTableStyle.td
-                        }
-                    >
+                                ? `${AcTableStyle.td} ${AcTableStyle.wideColumn}`
+                                : AcTableStyle.td
+                            }>
                         {row[key]}
                     </td>
                 ))}
@@ -42,34 +44,56 @@ const TableBody = ({ data, keys }) => (
 );
 
 
-const CustomTable = ({headers, keys, data}) => (
+const CustomTable = ({headers, keys, data, page, limit}) => (
     <table className={AcTableStyle.table}>
         <TableHeader headers={headers}/>
-        <TableBody data={data} keys={keys}/>
+        <TableBody data={data} keys={keys} page={page} limit={limit}/>
     </table>
 );
 
 const TableView = ({url, headers, keys}) => {
     const [data, setData] = useState([]);
+    const [page, setPage] = useState(1);
+    const [totalCount, setTotalCount] = useState(0);
+    const limit = 6;
 
     useEffect(() => {
+        
+        if (!url) return;
         const fullUrl = `${process.env.REACT_APP_API_BASE_URL}${url}`;
         console.log("호출 URL:", fullUrl);
-        if (!url) return;
+        
         axios
-            .get(`${process.env.REACT_APP_API_BASE_URL}${url}`)
+            .get(fullUrl, {
+                params: {
+                    page,
+                    perPage : limit,
+                },
+            })
             .then((res) => {
                 console.log("데이터:", res.data);
                 setData(res.data.announcements);
+                setTotalCount(res.data.totalCount);
             }
             )
             .catch((err) => console.error("데이터 요청 실패 ", err));
-    }, [url]);
+    }, [url, page]);
 
     return (
-        <div className={AcTableStyle.tableWrapper}>
-            <CustomTable headers={headers} keys={keys} data={data}/>
+       <div className={AcTableStyle.outerWrapper}>
+            <div className={AcTableStyle.tableWrapper}>
+                <CustomTable headers={headers} keys={keys} data={data} page={page} limit={limit} />
+            </div>
+            <div className={AcTableStyle.paginationWrapper}>
+                <Pagination
+                page={page}
+                limit={limit}
+                total={totalCount}
+                onPageChange={(newPage) => setPage(newPage)}
+                />
+            </div>
         </div>
+
     )
 
 }
