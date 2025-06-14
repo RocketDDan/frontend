b32bc67d47fd9044c8fb79ec7872d01ddfdd97e8
HideOnCodec <tlsdmsgp33@naver.com>
2025-06-14 23:02:56 +0900

    fix: 공통 컴포넌트 파라미터 설명 추가

--- src/components/base/CheckModal.jsx
+++ src/components/base/CheckModal.jsx
@@ -3,6 +3,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import styles from "./CheckModal.module.css";
 
+/**
+ * 
+ * @param {String} title
+ * @param {String} description
+ * @param {Function} onConfirm 확인 버튼 클릭 시 이벤트
+ * @param {Function} onClose 모달 닫기 버튼 클릭 시 이벤트
+ * @returns 
+ */
 const CheckModal = ({
   title,
   description,
--- src/components/crew/CrewMemberInfo.jsx
+++ src/components/crew/CrewMemberInfo.jsx
@@ -1,6 +1,13 @@
 import styles from './CrewMemberInfo.module.css'; 
 import { CrewMemberProfileImage } from '../profile/ProfileImage';
 
+/**
+ * 
+ * @param {String} profilePath 프로필 이미지 경로
+ * @param {String} nickname 크루 멤버의 닉네임
+ * @param {String} date 날짜
+ * @returns 
+ */
 const CrewMemberInfo = ({ profilePath, nickname, date }) => {
     return (
         <div className={styles.memberInfo}>
