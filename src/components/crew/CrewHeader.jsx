6371e5e86770373b0c5a2228b22eef8864eb9a90
HideOnCodec <tlsdmsgp33@naver.com>
2025-06-14 23:02:56 +0900

    fix: 공통 컴포넌트 파라미터 설명 추가

--- src/components/crew/CrewHeader.jsx
+++ src/components/crew/CrewHeader.jsx
@@ -1,5 +1,11 @@
 import styles from './CrewHeader.module.css';
 
+/**
+ * 
+ * @param {Array} columns - 각 열의 정보 객체 배열, 각 객체는 label, width 속성을 가질 수 있음 
+ * 예시 : [{ label: "이름", width: "160px" }, { label: "닉네임", width: "160px" }]
+ * @returns 
+ */
 const CrewHeader = ({ columns }) => {
     return (
         <div className={styles.headerWrapper}>
