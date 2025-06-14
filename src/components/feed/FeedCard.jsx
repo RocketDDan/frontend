36e1b8ab0566338dcd5f2427d03bbdb1bf41c6c5
JaeY0ungJung <hanol5216@gmail.com>
2025-06-15 02:10:16 +0900

    fix: 댓글 창 인스타와 비슷하게 수정 & 모바일에서 피드와 댓글창 중 하나만 보이도록 수정

--- src/components/feed/FeedCard.jsx
+++ src/components/feed/FeedCard.jsx
@@ -131,7 +131,6 @@ const FeedCard = ({ feed, onCommentClick }) => {
             </div>
 
             {/* 댓글 */}
-
             <div className={style.commentList}>
                 {/* 최대 3개만 보여주기 */}
                 {feed.commentList.map(comment => {
