547686ae2cbbc938d4735d4653e62a232627fc8d
KIMA <onionlily123@naver.com>
2025-06-17 10:57:37 +0900

    feat: 특정 회원 정보 조회 api 구현

--- src/api/member.api.js
+++ src/api/member.api.js
@@ -1,17 +1,13 @@
 import apiClient from "./apiClient";
 
-// 로그인한 회원 정보 조회 (GET /members/personal-info)
-const fetchMemberInfo = async () => {
-    try {
-        const response = await apiClient.get("/members/personal-info");
-        if (response.status !== 200) {
-            throw new Error("회원 정보 조회 실패", response.data);
-        }
-        console.log("회원 정보 조회 성공", response.data);
-        return response.data;
-    } catch (error) {
-        throw error;
-    }
+// 회원 정보 조회 (GET /members/{memberId})
+const fetchMemberInfo = async (memberId) => {
+  const response = await apiClient.get(`/members/${memberId}`);
+  if (response.status !== 200) {
+    throw new Error("회원 정보 조회 실패", response.data);
+  }
+  console.log("회원 정보 조회 성공", response.data);
+  return response.data;
 };
 
 export { fetchMemberInfo };
