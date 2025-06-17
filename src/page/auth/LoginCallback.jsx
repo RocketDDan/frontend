afaf0829ed70c5d8f9697185b2482013d6b40444
KIMA <onionlily123@naver.com>
2025-06-17 23:41:14 +0900

    fix: 공통 에러 처리

--- src/page/auth/LoginCallback.jsx
+++ src/page/auth/LoginCallback.jsx
@@ -16,17 +16,15 @@ const LoginCallback = () => {
       try {
         const user = await fetchUserInfo();
         setUser(user);
-        try {
-          const myCrewId = await fetchMyCrew();
-          if (myCrewId) {
-            const myCrewInfo = await fetchCrew(myCrewId);
-            setUserCrew({
-              crewId: myCrewInfo.crewId,
-              crewName: myCrewInfo.crewName,
-              isLeader: myCrewInfo.leader,
-            });
-          }
-        } catch (error) {}
+        const myCrewId = await fetchMyCrew();
+        if (myCrewId) {
+          const myCrewInfo = await fetchCrew(myCrewId);
+          setUserCrew({
+            crewId: myCrewInfo.crewId,
+            crewName: myCrewInfo.crewName,
+            isLeader: myCrewInfo.leader,
+          });
+        }
         navigate("/");
       } catch (error) {
         navigate("/login");
