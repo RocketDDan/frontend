24dc9344062fba9263ba76409540b35dcc82d251
JaeY0ungJung <hanol5216@gmail.com>
2025-06-18 14:32:25 +0900

    style: 공지 스타일 수정

--- src/page/announcement/AnnouncementListPage.jsx
+++ src/page/announcement/AnnouncementListPage.jsx
@@ -1,82 +1,92 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
-import styles from "./AnnouncementListPage.module.css";
+import style from "./AnnouncementListPage.module.css";
 import { SearchBar } from "../../components/search_bar/SearchBar";
 import { SecondaryButton } from "../../components/base/Button";
 import { TableView } from "../../components/base/AnnouncementTable";
 import { useNavigate } from "react-router-dom";
 import apiClient from "../../api/apiClient"
+import Pagination from "../../components/announcement/Pagination";
 
 const AnnouncementListPage = () => {
-  const navigate = useNavigate();
-  const [keyword, setKeyword] = useState("");
-  const [page, setPage] = useState(1);
-  const [data, setData] = useState([]);
-  const [totalCount, setTotalCount] = useState(0);
-  const limit = 6;
+	const navigate = useNavigate();
+	const [keyword, setKeyword] = useState("");
+	const [page, setPage] = useState(1);
+	const [data, setData] = useState([]);
+	const [totalCount, setTotalCount] = useState(0);
+	const limit = 6;
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        console.log(`${process.env.REACT_APP_API_BASE_URL}/announcements`);
-        // const res = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/announcements`, {
-        //   params: { page, perPage: limit, keyword }
-        // });
-        const res = await apiClient.get("/announcements", {
-          params: {
-            page,
-            perPage: limit,
-            keyword
-          }
-        });
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				console.log(`${process.env.REACT_APP_API_BASE_URL}/announcements`);
+				// const res = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/announcements`, {
+				//   params: { page, perPage: limit, keyword }
+				// });
+				const res = await apiClient.get("/announcements", {
+					params: {
+						page,
+						perPage: limit,
+						keyword
+					}
+				});
 
-        //const res = await apiClient.get(`/announcements`);
-        console.log("응답:", res.data);
+				//const res = await apiClient.get(`/announcements`);
+				console.log("응답:", res.data);
 
-        setData(res.data.announcements);
-        setTotalCount(res.data.totalCount);
-      } catch (err) {
-        console.error("데이터 요청 실패", err);
-      }
-    };
-    fetchData();
-  }, [page, keyword]);
+				setData(res.data.announcements);
+				setTotalCount(res.data.totalCount);
+			} catch (err) {
+				console.error("데이터 요청 실패", err);
+			}
+		};
+		fetchData();
+	}, [page, keyword]);
 
-  const handleChange = (val) => {
-    setKeyword(val);
-    setPage(1);
-  };
+	const handleChange = (val) => {
+		setKeyword(val);
+		setPage(1);
+	};
 
-  return (
-    <div className={styles.container}>
-      <h1>공지 사항</h1>
-      <div className={styles.topBar}>
-        <div style={{ width: "300px" }}>
-          <SearchBar
-            placeholder="검색하기"
-            value={keyword}
-            onChange={handleChange}
-            onEnter={() => console.log("엔터")}
-          />
-        </div>
-        <SecondaryButton
-          width="160px"
-          content="새 공지사항 등록"
-          onClick={() => navigate("/announcement/upload")}
-        />
-      </div>
-      <TableView
-        headers={["번호", "제목", "작성자", "작성날짜"]}
-        keys={["title", "crewName", "createdAt"]}
-        data={data}
-        page={page}
-        limit={limit}
-        totalCount={totalCount}
-        setPage={setPage}
-        onRowClick={(row) => navigate(`/announcement/${row.announcementId}/detail`)}
-      />
-    </div>
-  );
+	return (
+		<div className={style.container}>
+
+			<h1>공지 사항</h1>
+			{/* 위 */}
+			<div className={style.topBar}>
+				<SearchBar
+					placeholder="검색하기"
+					value={keyword}
+					onChange={handleChange}
+					width="15rem"
+					onEnter={() => console.log("엔터")}
+				/>
+				<SecondaryButton
+					width="7rem"
+					content="새 공지 등록"
+					onClick={() => navigate("/announcement/upload")}
+				/>
+			</div>
+			{/* 중간 */}
+			<div style={{ flex: 1 }} className={style.scrollX}>
+				<TableView
+					headers={["번호", "제목", "작성자", "작성날짜"]}
+					keys={["title", "crewName", "createdAt"]}
+					data={data}
+					page={page}
+					limit={limit}
+					height="fit-content"
+					onRowClick={(row) => navigate(`/announcement/${row.announcementId}/detail`)}
+				/>
+			</div>
+			{/* 아래 */}
+			<Pagination
+				page={page}
+				limit={limit}
+				total={totalCount}
+				onPageChange={setPage}
+			/>
+		</div>
+	);
 };
 
 export default AnnouncementListPage;
