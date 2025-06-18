aef0fe1bb4de7a47bf32948e3a0d8c0ec0f5d3de
JaeY0ungJung <hanol5216@gmail.com>
2025-06-18 16:15:44 +0900

    style: 피드 작성자 프로필 크기 수정

--- src/components/profile/ProfileImage.jsx
+++ src/components/profile/ProfileImage.jsx
@@ -8,11 +8,15 @@ import ProfileImageStyle from './ProfileImage.module.css';
  * 유저 프로필 이미지 컴포넌트
  * @component
  * @param {string} profileUrl  프로필 이미지 URL (없으면 기본 이미지 사용)
- * @param {string} size 가로/세로 길이 (헤더: 30px, 피드 작성자: 30px, 회원가입: 30px, 크루 관련 멤버 프로필: 40px, 멤버 프로필: 100px?, 크루 프로필: 200px?)
+ * @param {string} size 가로/세로 길이 (헤더: 30px, 피드 작성자: 40px, 회원가입: 30px, 크루 관련 멤버 프로필: 40px, 멤버 프로필: 100px?, 크루 프로필: 200px?)
  * @param {Function} onClick 클릭 시 이벤트
  * @returns {JSX.Element} 프로필 이미지 컴포넌트
  */
-const ProfileImage = ({ profileUrl, size = "30px", onClick }) => {
+const ProfileImage = ({
+	profileUrl,
+	size = "30px",
+	onClick 
+}) => {
 
 	const handleClick = (e) => {
 		onClick?.(e);
